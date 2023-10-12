02b03f0d5b2f7fc060f5d1b735b58808b74c99c3
lm-hossain <aleam.hossain@mymdoc.com>
2023-10-12 20:55:59 +0600

    fb long live access token

--- utils/token.js
+++ utils/token.js
@@ -3,7 +3,7 @@ const fbLongLiveAccess =async  (token)=>{
     console.log(token);
     return new Promise(async(resolve, reject) =>{
         try {
-            const access_token = await axios.get(`https://graph.facebook.com/v18.0/oauth/access_token?grant_type=fb_exchange_token&client_id=5109107729167022&client_secret=dc4dc755d731a7bfd189b4fd54b9176e&fb_exchange_token=${token}`)
+            const access_token = await axios.get(`${process.env.FACEBOOK_API_URL}/oauth/access_token?grant_type=fb_exchange_token&client_id=5109107729167022&client_secret=dc4dc755d731a7bfd189b4fd54b9176e&fb_exchange_token=${token}`)
             resolve(access_token.data)
         } catch (error) {
             reject(error)
