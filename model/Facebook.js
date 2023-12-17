f2fe9246fbbc8f45d7dc2e62f8717ffe5e6ccbaf
lm-hossain <aleam.hossain@mymdoc.com>
2023-12-17 19:36:50 +0600

    code indentation

--- model/Facebook.js
+++ model/Facebook.js
@@ -1,35 +1,35 @@
 const {Schema,model} = require('mongoose');
 
 const facebookSchema = new Schema({
-        id:String,
-        userId:{
-          type:Schema.Types.ObjectId,
-          ref:'User',
-        },
-        fbEmail:String,
-        accessToken:String,
-        name:String,
+  id:String,
+  userId:{
+    type:Schema.Types.ObjectId,
+    ref:'User',
+  },
+  fbEmail:String,
+  accessToken:String,
+  name:String,
+  profilePic:String,
+  pages:[{
+      pageName:String,
+      profilePic:String,
+      id:String,
+      pageCategory:String,
+      accessToken:String,
+      instagram:{
+        permission:Boolean,
+        instagramName:String,
         profilePic:String,
-        pages:[{
-            pageName:String,
-            profilePic:String,
-            id:String,
-            pageCategory:String,
-            accessToken:String,
-            instagram:{
-              permission:Boolean,
-              instagramName:String,
-              profilePic:String,
-              id:String,
-              accessToken:String,
-            }
-          }],
-        groups:[{
-            groupName:String,
-            profilePic:String,
-            id:String,
-            administrator:Boolean,
-        }],
+        id:String,
+        accessToken:String,
+      }
+    }],
+  groups:[{
+      groupName:String,
+      profilePic:String,
+      id:String,
+      administrator:Boolean,
+  }],
 });
 
 
