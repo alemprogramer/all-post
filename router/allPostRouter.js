ed8a21afdda8045d42566722e2bd813b5bfafc45
lm-hossain <aleam.hossain@mymdoc.com>
2023-12-21 17:50:54 +0600

    post validator  added

--- router/allPostRouter.js
+++ router/allPostRouter.js
@@ -50,9 +50,11 @@ const {facebookPostController} = require('../controller/facebookController');
 const {postInLinkedInController} = require('../controller/linkedinController');
 const {twitterTweetPostController} = require('../controller/twitterController');
 
+const {postValidator} = require('../validator/postValidator');
+
 const uploader = require('../middleware/uploader')
 
-router.post('/all',uploader.array('photos', 100),facebookPostController,postInLinkedInController,twitterTweetPostController,(req,res) => {
+router.post('/all',uploader.array('photos', 100),postValidator,facebookPostController,postInLinkedInController,twitterTweetPostController,(req,res) => {
     // console.log('image',req.files);
     res.status(200).json({
         status: 200,
