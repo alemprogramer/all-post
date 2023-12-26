fd9e0645e0c01326d449e5310f9b37f78e10c177
lm-hossain <aleam.hossain@mymdoc.com>
2023-12-26 13:29:49 +0600

    twitter filed added

--- model/User.js
+++ model/User.js
@@ -19,6 +19,8 @@ const userSchema = new Schema(
       twitterAccessToken: String,
       id:String,
       twitterRefreshToken: String,
+      accessTokenExpire: Number,
+      twitterProfile: String,
     },
     
     
