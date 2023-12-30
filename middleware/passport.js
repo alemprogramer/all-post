723cf995fec9cba6f522c8d1764fb33cc5336daf
lm-hossain <aleam.hossain@mymdoc.com>
2023-12-30 17:57:29 +0600

    change the facebook/callback

--- middleware/passport.js
+++ middleware/passport.js
@@ -8,7 +8,7 @@ const FacebookStrategy = require('passport-facebook').Strategy;
 passport.use(new FacebookStrategy({
   clientID: `5109107729167022`,
   clientSecret: `dc4dc755d731a7bfd189b4fd54b9176e`,
-  callbackURL: 'http://localhost:3000/api/v1/facebook/callback', // Your callback URL
+  callbackURL: process.env.FACEBOOK_CALLBACK_URL, // Your callback URL
   profileFields: ['id', 'displayName', 'email','profileUrl','picture'],
   scope:['pages_show_list','pages_read_user_content','pages_show_list', 'instagram_basic', 'instagram_content_publish','public_profile','email'],
   state: true
