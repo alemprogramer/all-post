e161959b426acd3f816f3bc6490101dc090f972b
lm-hossain <aleam.hossain@mymdoc.com>
2025-03-15 00:23:51 +0600

    router and controller fixed fb login

--- controller/facebookController.js
+++ controller/facebookController.js
@@ -6,16 +6,16 @@ const FaceBook = require('../model/Facebook');
 const {
     createRefreshToken,
     createAccessToken,
-  } = require("../utils/tokenCreate");
+} = require("../utils/tokenCreate");
 const axios = require('axios');
 const Facebook = require('../model/Facebook');
-const {fileUploadOnFacebookPAge,instagramImageUrlForPost} = require('../utils/facebookFileUpload');
+const { fileUploadOnFacebookPAge, instagramImageUrlForPost } = require('../utils/facebookFileUpload');
 
 const sdk = new SuperfaceClient();
 
-exports.fbLongLiveAccessTokenController = async (req,res,next)=>{
+exports.fbLongLiveAccessTokenController = async (req, res, next) => {
     try {
-        const token =await fbLongLiveAccess('EABImtNdQ8q4BOyXl78V6Y41YkO3AoMK11ZA5zZAA875ZC4D3NDzLvikT7vbNJViKQop47QJMZAyrEPJrYkouykjKyBC5Cn4BSqBbBP6hOBu1gXJyGydZArmVZBKy4JE2hutz0P0gwiU9ZAmTQqcoZAMNQrnozJCmg6bEEE927dZCtxGaGIUyqbVhaNOOpwYAwEFF3GoGP6KsldgKq0kMRNtL1PMbOKFdpKS32eZANCfn1rpZC7U5YVQJ9geOCC0QbKPPkZBemQZDZD')
+        const token = await fbLongLiveAccess('EABImtNdQ8q4BOyXl78V6Y41YkO3AoMK11ZA5zZAA875ZC4D3NDzLvikT7vbNJViKQop47QJMZAyrEPJrYkouykjKyBC5Cn4BSqBbBP6hOBu1gXJyGydZArmVZBKy4JE2hutz0P0gwiU9ZAmTQqcoZAMNQrnozJCmg6bEEE927dZCtxGaGIUyqbVhaNOOpwYAwEFF3GoGP6KsldgKq0kMRNtL1PMbOKFdpKS32eZANCfn1rpZC7U5YVQJ9geOCC0QbKPPkZBemQZDZD')
         res.send(token)
     } catch (error) {
         next(error);
@@ -23,45 +23,33 @@ exports.fbLongLiveAccessTokenController = async (req,res,next)=>{
 }
 
 exports.fbLoginCallBackController = async function (req, res, next) {
-    
+
     try {
-      // <8> Obtaining profiles
-    //   console.log('long live token',await fbLongLiveAccess(req.user.accessToken));
-        const accessToken = req.body.user.access_token;
-        console.log("🚀 ~ file: facebookController.js:31 ~ req.user:", req.body.user)
-        console.log("🚀 ~ file: facebookController.js:31 ~ accessToken:", accessToken)
-        const sdkProfile = await sdk.getProfile(
-            'social-media/publishing-profiles@1.0.1'
+        
+        // get token form facebook
+        const tokenResponse = await axios.get(
+            `https://graph.facebook.com/v22.0/oauth/access_token?client_id=${process.env.FACEBOOK_APP_ID}&client_secret=${process.env.FACEBOOK_APP_SECRET}&redirect_uri=${process.env.FACEBOOK_CALLBACK_URL}&code=${req.query.code}`
         );
-        const result = await sdkProfile
-            .getUseCase('GetProfilesForPublishing')
-            .perform(
-            {},
-            {
-                provider: 'instagram',
-                parameters: {
-                accessToken,
-                },
-            }
-            );
+        const accessToken = await fbLongLiveAccess(tokenResponse.data.access_token);
+
+        const userData = await axios.get(`https://graph.facebook.com/v22.0/me?fields=email,name,picture&access_token=${accessToken}`);
+        const {email,name, picture,id} = userData.data
+        // console.log("🚀 ~ userData:", email, name, picture,id)
+        console.log('all ok');
+
         const facebookPageList = await axios.get(`${process.env.FACEBOOK_API_URL}/me/accounts?fields=instagram_business_account{name},picture,name,category,access_token&access_token=${accessToken}`)
-        const instagram = result.unwrap().profiles;
+       
 
         //new code 
         const pageList = facebookPageList.data.data;
-        const {id,displayName,photos} = req.body.user.profile
-        
+
         //page list formatter
         const listOfPage = []
-        for(let i = 0; i<pageList.length ; i++) {
+        for (let i = 0; i < pageList.length; i++) {
             if(pageList[i].instagram_business_account) {
-                let data ;
-                for(let j = 0; j < instagram.length; j++) {
-                    if(instagram[j].id == pageList[i].instagram_business_account.id){
-                        data = instagram[j];
-                        break;
-                    }
-                }
+                const {id,name} = pageList[i].instagram_business_account
+                const {profile_picture_url} = await axios.get(`${process.env.FACEBOOK_API_URL}/${id}?fields=profile_picture_url&access_token=${accessToken}`)
+
                 let obj = {
                     id:pageList[i].id,
                     pageName: pageList[i].name,
@@ -69,64 +57,63 @@ exports.fbLoginCallBackController = async function (req, res, next) {
                     pageCategory:pageList[i].category,
                     accessToken:pageList[i].access_token,
                     instagram:{
-                        instagramName:data?.name || "",
-                        profilePic:data?.imageUrl,
-                        id:data.id,
+                        instagramName:name || "",
+                        profilePic:profile_picture_url || "",
+                        id,
                         accessToken:accessToken,
                         permission:true,
                     }
                 }
                 listOfPage.push(obj)
-                
+
             }else{
-                let obj = {
-                    id:pageList[i].id,
-                    pageName: pageList[i].name,
-                    profilePic:pageList[i].picture.data.url,
-                    pageCategory:pageList[i].category,
-                    accessToken:pageList[i].access_token,
-                }
-                listOfPage.push(obj)
+            let obj = {
+                id: pageList[i].id,
+                pageName: pageList[i].name,
+                profilePic: pageList[i].picture.data.url,
+                pageCategory: pageList[i].category,
+                accessToken: pageList[i].access_token,
             }
-                
+            listOfPage.push(obj)
+            }
         }
 
         const currentDate = new Date();
         const ExpireDate = new Date(currentDate.getTime() + (1000 * 60 * 60 * 24 * 90)); //60 days
-        const isUser = await Facebook.findOne({id})
-        let userIdForToken = '' ;
+        const isUser = await Facebook.findOne({ id })
+        let userIdForToken = '';
         if (isUser) {
-            const {userId,_id} = isUser ;
+            const { userId, _id } = isUser;
             const update = {
                 $set: {
                     pages: listOfPage,
                     accessTokenExpire: ExpireDate,
                 }
             };
-            await Facebook.findOneAndUpdate({ _id:_id },update);
-            userIdForToken=userId
-          
+            await Facebook.findOneAndUpdate({ _id: _id }, update);
+            userIdForToken = userId
+
         } else {
             console.log('login');
 
             const facebook = new FaceBook({
-                id:id,
-                name:displayName,
+                id: id,
+                name: displayName,
                 accessToken: accessToken,
                 // profilePic: photos.value,
                 pages: listOfPage || [],
-                groups:[],
+                groups: [],
                 accessTokenExpire: ExpireDate
             })
 
             const fbUser = await facebook.save();
 
             const user = new User({
-                name:displayName,
-                linkedin:{},
-                facebook:[fbUser._id]
+                name: displayName,
+                linkedin: {},
+                facebook: [fbUser._id]
             })
-           
+
             let newUser = await user.save();
 
             const facebookData = await Facebook.findById(fbUser._id);
@@ -134,52 +121,47 @@ exports.fbLoginCallBackController = async function (req, res, next) {
             await facebookData.save();
             userIdForToken = newUser._id;
         }
-        const refresh_token = createRefreshToken({id: userIdForToken}, process.env.REFRESH_TOKEN_SECRET,'30d')
-        const access_token = createAccessToken({id: userIdForToken}, process.env.ACCESS_TOKEN_SECRET,'50m');
+        const refresh_token = createRefreshToken({ id: userIdForToken }, process.env.REFRESH_TOKEN_SECRET, '30d')
+        const access_token = createAccessToken({ id: userIdForToken }, process.env.ACCESS_TOKEN_SECRET, '50m');
 
-    
         //social media cookies
-       
+        res.redirect(`${process.env.LOGIN_REDIRECT_URL}?access_token=${access_token}&refresh_token=${refresh_token}&facebook_AccessToken=${accessToken}; expires=${new Date(Date.now() + 1000 * 60 * 60 * 24 * 90)}`)
 
-        
-        res.send(`${process.env.LOGIN_REDIRECT_URL}?access_token=${access_token}&refresh_token=${refresh_token}&facebook_AccessToken=${accessToken}; expires=${new Date(Date.now() + 1000 * 60 *60 *24*90)}`)
-
-        
     } catch (err) {
         console.log(err);
         next(err);
     }
-  }
+}
 
 exports.facebookGroupDataCollectController = async (req, res, next) => {
     try {
         console.log('id:', req.id);
         // const user = await User.findById(req.id).populate('facebook')
-        const facebook = await Facebook.findOne({userId: req.id})
+        const facebook = await Facebook.findOne({ userId: req.id })
         let allGroups = []
         let url = `${process.env.FACEBOOK_API_URL}/me/groups?fields=administrator,name,picture{url}&limit=100&access_token=${facebook.accessToken}`;
-        while(url){
+        while (url) {
             let response = await axios.get(url);
             let groups = response.data
-            for(let i = 0; i < groups.data.length; i++) {
-                if(groups.data[i].administrator){
+            for (let i = 0; i < groups.data.length; i++) {
+                if (groups.data[i].administrator) {
                     allGroups.push(groups.data[i])
                 }
             }
-            if(groups.paging.next){
+            if (groups.paging.next) {
                 url = groups.paging.next
-            }else {
-                url=""
+            } else {
+                url = ""
             }
         }
 
         let finalGroup = [];
-        for(let i = 0; i < allGroups.length; i++) {
+        for (let i = 0; i < allGroups.length; i++) {
             let obj = {
-                groupName:allGroups[i].name,
-                profilePic:allGroups[i].picture.data.url,
-                id:allGroups[i].id,
-                administrator:allGroups[i].administrator,
+                groupName: allGroups[i].name,
+                profilePic: allGroups[i].picture.data.url,
+                id: allGroups[i].id,
+                administrator: allGroups[i].administrator,
             }
             finalGroup.push(obj)
         }
@@ -188,10 +170,10 @@ exports.facebookGroupDataCollectController = async (req, res, next) => {
         await facebook.save();
 
         res.status(200).json({
-            status:200,
+            status: 200,
             message: 'Facebook Group data collected successfully',
             // user
-            groups:finalGroup
+            groups: finalGroup
         })
     } catch (error) {
         next(error);
@@ -199,66 +181,66 @@ exports.facebookGroupDataCollectController = async (req, res, next) => {
 }
 
 //TODO: message collect
-exports.facebookPostController = async (req, res,next) => {
+exports.facebookPostController = async (req, res, next) => {
     //facebook will be true if any page or group id in the array
-    let {facebook,facebookPageIds,facebookGroupsIds,instagramIds,text} =req.body;
+    let { facebook, facebookPageIds, facebookGroupsIds, instagramIds, text } = req.body;
     const { facebook: fb } = req.user;
 
-    if(!(Array.isArray(facebookPageIds)&&facebookPageIds.length > 0)){
+    if (!(Array.isArray(facebookPageIds) && facebookPageIds.length > 0)) {
         facebookPageIds = [facebookPageIds];
     }
 
-    if(!(Array.isArray(facebookGroupsIds)&&facebookGroupsIds.length > 0)){
+    if (!(Array.isArray(facebookGroupsIds) && facebookGroupsIds.length > 0)) {
         facebookGroupsIds = [facebookGroupsIds];
     }
 
-    if(!(Array.isArray(instagramIds)&&instagramIds.length > 0)){
+    if (!(Array.isArray(instagramIds) && instagramIds.length > 0)) {
         instagramIds = [instagramIds];
     }
 
     // var postId ;
-    
+
     try {
         //if facebook user not post in any fb platforms
-        if((facebook != 'true') || !fb.length) {
+        if ((facebook != 'true') || !fb.length) {
             console.log('fb is empty');
             //if user not selected any fb pages , groups and instagram accounts it will be go to next middleware
-            return  next();
+            return next();
         }
 
 
         let imageUrl;
-        if(Array.isArray(facebookPageIds)&&facebookPageIds[0]){
+        if (Array.isArray(facebookPageIds) && facebookPageIds[0]) {
             console.log('fb pages');
-            
+
             //loop for multiple facebook accounts
-            for(let i = 0; i < fb.length; i++) {
+            for (let i = 0; i < fb.length; i++) {
                 //get user all pages data 
                 //for new token for page post
                 const facebookPageList = await axios.get(`${process.env.FACEBOOK_API_URL}/me/accounts?fields=name,id,access_token&access_token=${fb[i].accessToken}`)
                 const facebookData = facebookPageList.data.data;
-                
+
                 //check token is valid or not
-                if(!facebookData.length){
+                if (!facebookData.length) {
                     return res.status(400).json({
-                        status:'400',
+                        status: '400',
                         message: 'please login facebook  again'
                     })
                 }
 
                 //post on user selected pages
                 const message = [];
-                for(let i=0; i < facebookData.length; i++){
+                for (let i = 0; i < facebookData.length; i++) {
                     //check witch pages are selected for page (by page id machine)
-                    if(facebookPageIds.includes(facebookData[i].id)){
+                    if (facebookPageIds.includes(facebookData[i].id)) {
                         let url = `${process.env.FACEBOOK_API_URL}/${facebookData[i].id}/feed?message=${text}&access_token=${facebookData[i].access_token}`
-                        if(req.files.length){
-                            let data = await  fileUploadOnFacebookPAge(facebookData[i].id,facebookData[i].access_token,req)
-                            if(Array.isArray(instagramIds)&&instagramIds.length){
-                                imageUrl = await instagramImageUrlForPost(data.data.post_id,facebookData[i].access_token)
+                        if (req.files.length) {
+                            let data = await fileUploadOnFacebookPAge(facebookData[i].id, facebookData[i].access_token, req)
+                            if (Array.isArray(instagramIds) && instagramIds.length) {
+                                imageUrl = await instagramImageUrlForPost(data.data.post_id, facebookData[i].access_token)
                             }
 
-                        }else{
+                        } else {
                             await axios.post(url)
                         }
                     }
@@ -267,37 +249,37 @@ exports.facebookPostController = async (req, res,next) => {
         }
 
         //TODO: message collect
-        if(Array.isArray(facebookGroupsIds)&&facebookGroupsIds[0]){
+        if (Array.isArray(facebookGroupsIds) && facebookGroupsIds[0]) {
             console.log('fb groups: ');
             //loop for multiple facebook accounts
-            for(let i = 0; i < fb.length; i++) {
+            for (let i = 0; i < fb.length; i++) {
                 //loop for post on selected facebook groups
-                for(let j = 0; j < facebookGroupsIds.length; j++) {
-                    
-                    if(req.files.length){
+                for (let j = 0; j < facebookGroupsIds.length; j++) {
+
+                    if (req.files.length) {
                         //image and test with post
-                        let data = await  fileUploadOnFacebookPAge(facebookGroupsIds[j],fb[i].accessToken,req)
-                        
-                            if(Array.isArray(instagramIds)&&instagramIds.length){
-                                imageUrl = await instagramImageUrlForPost(data.data.post_id,fb[i].accessToken)
-                            }
-                    }else{
+                        let data = await fileUploadOnFacebookPAge(facebookGroupsIds[j], fb[i].accessToken, req)
+
+                        if (Array.isArray(instagramIds) && instagramIds.length) {
+                            imageUrl = await instagramImageUrlForPost(data.data.post_id, fb[i].accessToken)
+                        }
+                    } else {
                         //without any image post
-                        let url =`${process.env.FACEBOOK_API_URL}/${facebookGroupsIds[j]}/feed?message=${text}&access_token=${fb[i].accessToken}`
+                        let url = `${process.env.FACEBOOK_API_URL}/${facebookGroupsIds[j]}/feed?message=${text}&access_token=${fb[i].accessToken}`
                         let response = await axios.post(url);
                     }
                 }
-                
+
             }
-            
+
         }
-        if(Array.isArray(instagramIds)&&instagramIds.length && req.files.length && false){ //TODO: add URL Shortener then is using
-            console.log('instagram',imageUrl);
-            console.log('text',text);
+        if (Array.isArray(instagramIds) && instagramIds.length && req.files.length && false) { //TODO: add URL Shortener then is using
+            console.log('instagram', imageUrl);
+            console.log('text', text);
             //loop for multiple facebook accounts
-            for(let i = 0; i < fb.length; i++) {
+            for (let i = 0; i < fb.length; i++) {
                 //loop for post on selected instagram account connected to facebook page
-                for(let j = 0; j < instagramIds.length; j++) { 
+                for (let j = 0; j < instagramIds.length; j++) {
                     console.log(`access_token=${fb[i].accessToken}`);
                     console.log(instagramIds[j]);
                     //single image  upload API 
--- router/facebookRouter.js
+++ router/facebookRouter.js
@@ -2,11 +2,12 @@ const router = require('express').Router();
 const passport = require('../middleware/passport')
 
 const { fbLongLiveAccessTokenController,fbLoginCallBackController,facebookPostController,facebookGroupDataCollectController } = require('../controller/facebookController');
+console.log('here');
 
 router.get('/groups',facebookGroupDataCollectController)
 router.get('/fb-refresh-token', fbLongLiveAccessTokenController);
 router.get('/login',passport.authenticate('facebook'));
-router.post('/callback',fbLoginCallBackController);
+router.get('/callback',fbLoginCallBackController);
 router.get('/post',facebookPostController);
 
 
